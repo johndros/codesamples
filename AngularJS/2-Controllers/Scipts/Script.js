1f9d24c546410488c0d9caa8712f3faffd7abfea
JohnDroS <mrx123456789@gmail.com>
2018-12-30 14:37:39 +0200

    .

---
+++ AngularJS/2-Controllers/Scipts/Script.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var employee = {
+            firstName: 'Mark',
+            lastName: 'Hastings',
+            gender: 'Male'
+        };
+    $scope.employee = employee;
+    });
